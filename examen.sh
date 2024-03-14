3ca69e19a6104cc4bd80abe238be9cb99ae5c13f
Comotellame07 <157026631+Comotellame07@users.noreply.github.com>
2024-03-14 17:56:42 +0100

    Update examen.sh

--- examen.sh
+++ examen.sh
@@ -14,26 +14,29 @@ echo -n "Escoger opcion: "
 read opcion
 if [  $opcion = "1" ]
 then
-  netplan
-elif [ $opcion = "2" ]
+  servicio
+elif [  $opcion = "2" ]
 then
-  ou1
+  netplan
 elif [ $opcion = "3" ]
 then
-  ou2
+  ou1
 elif [ $opcion = "4" ]
 then
-  gr
+  ou2
 elif [ $opcion = "5" ]
 then
-  usr
+  gr
 elif [ $opcion = "6" ]
 then
-  nfs
+  usr
 elif [ $opcion = "7" ]
 then
-  movil
+  nfs
 elif [ $opcion = "8" ]
+then
+  movil
+elif [ $opcion = "9" ]
 then
   echo "Saliendo del programa..."
 else
@@ -42,11 +45,105 @@ else
 fi
 }
 
-netplan() {
-    clear
-    echo "Ejecutando netplan..."
-    ./rs/netplan.sh
+###################################################################################
+###################################################################################
+
+servicio() {
+
 }
+
+###################################################################################
+###################################################################################
+
+netplan() {
+staticip() {
+read -p "IP Estática Ej. 192.168.100.10/24: " staticip
+read -p "¿Estas seguro?(y/n)" resp
+if [ $resp = "y" ]
+then
+echo "OK"
+elif [ $resp = "n" ]
+then
+staticip
+else
+staticip
+fi
+}
+
+gatewayip() {
+read -p "IP router: " gatewayip
+read -p "¿Estas seguro?(y/n)" resp
+if [ $resp = "y" ]
+then
+echo "OK"
+elif [ $resp = "n" ]
+then
+gatewayip
+else
+gatewayip
+fi
+}
+
+nameserversip() {
+read -p "Servidores DNS: " nameserversip
+read -p "¿Estas seguro?(y/n)" resp
+if [ $resp = "y" ]
+then
+echo "OK"
+elif [ $resp = "n" ]
+then
+nameserversip
+else
+nameserversip
+fi
+}
+
+netplan() {
+read -p "¿Quieres DHCP activo?(y/n): " dhcp
+if [ $dhcp = y ]
+then
+cat > /etc/netplan/00-installer-config.yaml <<EOF
+network:
+  version: 2
+  ethernets:
+    $nic
+      dhcp4: true
+EOF
+sudo netplan apply
+elif [ $dhcp = n ]
+then
+staticip
+gatewayip
+nameserversip
+dominio
+echo
+cat > /etc/netplan/00-installer-config.yaml <<EOF
+network:
+  version: 2
+  ethernets:
+    $nic
+      dhcp4: false
+      addresses:
+      - $staticip
+      gateway4: $gatewayip
+      nameservers:
+       addresses: [$nameserversip]
+EOF
+sudo netplan apply
+else
+clear
+echo "Repitelo, no se te entiende"
+netplan
+fi
+}
+
+nic=`ifconfig | awk 'NR==1{print $1}'`
+netplan
+echo "Netplan configurado"
+}
+
+###################################################################################
+###################################################################################
 
 ou1() {
     clear
