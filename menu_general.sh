5cfea5fe54c50f203484b15cc557728f5b423acb
Comotellame07 <157026631+Comotellame07@users.noreply.github.com>
2024-03-14 17:14:41 +0100

    Update and rename rs/server.sh to menu_general.sh

---
+++ menu_general.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+menu() {
+echo "Realizado por Dario Moreno"
+echo "Bienvenido al menu de administración de SOR en Ubuntu"
+echo "1. Configurar Netplan"
+echo "2. Agregar unidad organizativa de 1ºnivel"
+echo "3. Agregar unidad organizativa de 2ºnivel"
+echo "4. Agregar grupo"
+echo "5. Agregar usuario"
+echo "6. Carpeta compartida"
+echo "7. Perfil movil"
+echo "8. Salir"
+echo -n "Escoger opcion: "
+read opcion
+if [  $opcion = "1" ]
+then
+  netplan
+elif [ $opcion = "2" ]
+then
+  ssh
+elif [ $opcion = "3" ]
+then
+  ftp
+elif [ $opcion = "4" ]
+then
+  dns
+elif [ $opcion = "5" ]
+then
+  dns
+elif [ $opcion = "6" ]
+then
+  dns
+elif [ $opcion = "7" ]
+then
+  dns
+elif [ $opcion = "8" ]
+then
+  echo "Saliendo del programa..."
+else
+  clear
+  menu
+fi
+}
+
+netplan() {
+    clear
+    echo "Ejecutando netplan..."
+    ./sh/netplan.sh
+    sleep 1
+}
